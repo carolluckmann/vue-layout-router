712968aaed1eab41365f19899cb4327a9697b16b
carolluckmann <carolinaluckmann05@gmail.com>
2023-06-26 16:50:24 -0300

    commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
   <header>HEADER</header>
-  <aside>MENU</aside>
-  <main>MAIN</main>
+  <aside>
+    <nav class="menu">
+      <router-link to ="/">Home</router-link>
+      <router-link to ="/about">About</router-link>
+      <router-link to ="/contact">Contact</router-link>
+      <router-link to ="/news">News</router-link>
+      <router-link to ="/team">Team</router-link>
+    </nav>
+  </aside>
+  <main>
+    <router-view/>
+  </main>
   <footer>FOOTER</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.menu{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
